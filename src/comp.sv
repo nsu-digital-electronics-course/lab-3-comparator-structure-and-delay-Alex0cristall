c5f59d972b751a870a68e4f0af6102d1491757dd
Александр Попов <129597124+Alex0cristall@users.noreply.github.com>
2026-03-17 23:06:13 +0700

    Update comp.sv

--- src/comp.sv
+++ src/comp.sv
@@ -8,7 +8,45 @@ module comp(
     output logic lt
     );
     
-// TODO
-// Вот тут описываем свой компаратор  
+
+    logic [31:0] bit_gt; 
+    logic [31:0] bit_lt; 
+    logic [31:0] bit_eq;  
+
+    genvar i;
+    generate
+        for (i = 0; i < 32; i++) begin : bit_comp
+            assign bit_gt[i] =  a[i] & ~b[i];
+            assign bit_lt[i] = ~a[i] &  b[i];
+            assign bit_eq[i] = a[i] ^~ b[i];
+        end
+    endgenerate
+
+
+    logic [31:0] higher_eq;
+
+    assign higher_eq[31] = 1'b1;  
+
+    generate
+        for (i = 30; i >= 0; i--) begin : higher_eq_chain
+            assign higher_eq[i] = higher_eq[i+1] & bit_eq[i+1];
+        end
+    endgenerate
+
+
+    logic [31:0] first_gt;
+    logic [31:0] first_lt;
+
+    generate
+        for (i = 0; i < 32; i++) begin : first_diff
+            assign first_gt[i] = higher_eq[i] & bit_gt[i];
+            assign first_lt[i] = higher_eq[i] & bit_lt[i];
+        end
+    endgenerate
+
+   
+    assign gt = |first_gt;       
+    assign lt = |first_lt;       
+    assign eq = ~(gt | lt);      
     
 endmodule
